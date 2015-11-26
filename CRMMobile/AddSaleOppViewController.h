832579240547c1bc301850451a7b8dad29f0d6cd
liuguojiang666888 <1216021840@qq.com>
2015-11-26 10:19:23 +0800

    12

---
+++ CRMMobile/AddSaleOppViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddSaleOppViewController.h
+//  CRMMobile
+//
+//  Created by jam on 15/11/9.
+//  Copyright (c) 2015年 dagong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SaleOppEntity.h"
+
+@interface AddSaleOppViewController : UIViewController
+@property (strong, nonatomic) SaleOppEntity *saleOppEntity;
+@end
