133f2fe8fd39bdfce1f44c397e6eda7f514765e8
liuguojiang666888 <1216021840@qq.com>
2015-11-26 10:08:44 +0800

    1234

--- CRMMobile/marketActivity.h
+++ CRMMobile/marketActivity.h
@@ -9,5 +9,14 @@
 #import <Foundation/Foundation.h>
 
 @interface marketActivity : NSObject
+
 @property (strong,nonatomic) NSString *activityName;
+@property (strong,nonatomic) NSString *activityDate;
+@property (strong,nonatomic) NSString *activityAddress;
+@property (strong,nonatomic) NSString *activityCost;
+@property (strong,nonatomic) NSString *activityContent;
+@property (strong,nonatomic) NSString *responsibleDepartmentStr;
+@property (strong,nonatomic) NSString *responsibleDepartmentPersonStr;
+@property (strong,nonatomic) NSString *activitySketch;
+
 @end
